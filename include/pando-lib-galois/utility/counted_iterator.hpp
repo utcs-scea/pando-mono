df1150d1751265150e47b81b4f09eb547ec8b62d
AdityaAtulTewari <adityaatewari@gmail.com>
2024-01-18 18:06:20 -0800

    counted iterator should be addable (#143)

--- include/pando-lib-galois/utility/counted_iterator.hpp
+++ include/pando-lib-galois/utility/counted_iterator.hpp
@@ -83,6 +83,10 @@ struct CountedIterator {
     return tmp;
   }
 
+  CountedIterator<It> operator+(std::uint64_t n) {
+    return CountedIterator<It>{m_count + n, m_iter + n};
+  }
+
   friend bool operator==(const CountedIterator<It>& a, const CountedIterator<It>& b) {
     return a.m_count == b.m_count && a.m_iter == b.m_iter;
   }
