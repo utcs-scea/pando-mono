a0114d5ed886888e6cf7de25ecc6a4e5b897b287
AdityaAtulTewari <adityaatewari@gmail.com>
2024-01-18 22:03:19 -0800

    updated drv run scripts (#144)
    * updated scripts
    
    * updated scripts

--- scripts/run-drv.sh
+++ scripts/run-drv.sh
@@ -3,6 +3,25 @@
 # Copyright (c) 2023. University of Texas at Austin. All rights reserved.
 # Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+#
+# Help function
+#
+function show_help() {
+	echo "
+usage: preprun -n <n> prog [program args]
+    options:
+      -n <n> number of emulated PXNs
+      -c <n> number of emulated cores per PXN
+      -t <n> number of emulated harts per core
+      -p <n> number of host threads
+      -h     this help message
+"
+}
+
 HOST_THREADS="${HOST_THREADS:-16}"
 HOSTS="${HOSTS:-1}"
 PROCS="${PROCS:-2}"
@@ -12,15 +31,41 @@ THREADD=$((${HOST_THREADS} * ${HOSTS} / ${PROCS}))
 THREADS="${THREADS:-${THREADD}}"
 # 8GB Main memory size by default
 MAIN_MEMORY_SIZE="${MAIN_MEMORY_SIZE-8589934592}"
-LAUNCH_SCRIPT="${LAUNCH_SCRIPT:-pando-drv/tests/PANDOHammerDrvX.py}"
-EXE="${EXE:-dockerbuild-drv/pando-rt/examples/libhelloworld.so}"
+LAUNCH_DIR="${LAUNCH_DIR:-$(realpath .)}"
+LAUNCH_SCRIPT="${LAUNCH_SCRIPT:-${LAUNCH_DIR}/pando-drv/tests/PANDOHammerDrvX.py}"
+
+while getopts "p:n:c:t:h" option; do
+	case ${option} in
+	p) # host threads
+		HOST_THREADS=${OPTARG}
+		;;
+
+	n) # number of emulated PXNs
+		HOSTS=${OPTARG}
+		;;
+	c) # number of emulated cores per PXN
+		CORES=${OPTARG}
+		;;
+	t) # number of emulated cores per PXN
+		HARTS=${OPTARG}
+		;;
+	h) # help
+		show_help
+		exit
+		;;
+	esac
+done
+
+shift $(expr $OPTIND - 1)
+PROG=$1
+shift
 
 sst -n ${HOST_THREADS} \
 	"${LAUNCH_SCRIPT}" -- \
-	--with-command-processor="${EXE}" \
+	--with-command-processor="${PROG}" \
 	--num-pxn=${PROCS} \
 	--pod-cores=${CORES} \
 	--core-threads=${HARTS} \
 	--drvx-stack-in-l1sp \
 	--pxn-dram-size=${MAIN_MEMORY_SIZE} \
-	"${EXE}"
+	${PROG} $@
