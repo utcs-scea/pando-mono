688d9e4091813d3972a89a685431866646e5da3b
patrickkenney9801 <patrickkenney9801@gmail.com>
2024-04-04 09:33:02 -0500

    chore: Fix cmake linker issues

--- cmake/PANDOTesting.cmake
+++ cmake/PANDOTesting.cmake
@@ -118,7 +118,7 @@ function(pando_add_driver_test_lib TARGET SOURCEFILE LIBRARY)
 endfunction()
 
 function(pando_add_driver_test TARGET SOURCEFILE)
-  pando_add_driver_test_lib(${TARGET} ${SOURCEFILE} pando-lib-galois::pando-lib-galois)
+  pando_add_driver_test_lib(${TARGET} ${SOURCEFILE} "")
 endfunction()
 
 function(pando_add_bin_test TARGET ARGS INPUTFILE OKFILE)
