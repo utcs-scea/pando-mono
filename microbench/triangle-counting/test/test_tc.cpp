36cbfe7c61cf9caa3167099528329633dd1bf906
Prachi Ingle <prachi.ingle@utexas.edu>
2024-04-11 16:38:07 +0000

    Tests Build and Run correctly

--- microbench/triangle-counting/test/test_tc.cpp
+++ microbench/triangle-counting/test/test_tc.cpp
@@ -12,55 +12,63 @@ uint64_t get_expected_TC(const std::string okFile) {
   return expected_tc;
 }
 
-void e2e_tc_test(uint64_t expected_tc, pando::Array<char> filename, uint64_t numVertices,
-                 bool load_balanced_graph) {
+void e2e_tc_test(uint64_t expected_tc, pando::Array<char> filename, uint64_t num_vertices,
+                 bool load_balanced_graph, TC_CHUNK tc_chunk) {
   galois::DAccumulator<uint64_t> final_tri_count;
   EXPECT_EQ(final_tri_count.initialize(), pando::Status::Success);
   pando::Notification necessary;
   PANDO_CHECK(pando::executeOn(pando::Place{pando::NodeIndex{0}, pando::anyPod, pando::anyCore},
-                               &HBMainTC, necessary.getHandle(), filename, numVertices,
-                               load_balanced_graph, final_tri_count));
+                               &HBMainTC, necessary.getHandle(), filename, num_vertices,
+                               load_balanced_graph, tc_chunk, final_tri_count));
   necessary.wait();
   EXPECT_EQ(final_tri_count.reduce(), expected_tc);
   final_tri_count.deinitialize();
 }
 
-class TriangleCount
-    : public ::testing::TestWithParam<std::tuple<const char*, const char*, std::uint64_t>> {};
-TEST_P(TriangleCount, BasicDL) {
+class TriangleCountChunking
+    : public ::testing::TestWithParam<std::tuple<const char*, uint64_t, uint64_t, TC_CHUNK>> {};
+TEST_P(TriangleCountChunking, BasicDL) {
   const std::string elFile = std::get<0>(GetParam());
-  const std::string okFile = std::get<1>(GetParam());
-  const std::uint64_t numVertices = std::get<2>(GetParam());
-
-  uint64_t expected_tc = get_expected_TC(okFile);
+  const uint64_t num_vertices = (uint64_t)std::get<1>(GetParam());
+  const uint64_t expected_tc = std::get<2>(GetParam());
+  TC_CHUNK tc_chunk = std::get<3>(GetParam());
 
   pando::Array<char> filename;
   EXPECT_EQ(pando::Status::Success, filename.initialize(elFile.size()));
   for (uint64_t i = 0; i < elFile.size(); i++)
     filename[i] = elFile[i];
 
-  e2e_tc_test(expected_tc, filename, numVertices, true);
-
-  filename.deinitialize();
-}
-
-TEST_P(TriangleCount, BasicDA) {
-  const std::string elFile = std::get<0>(GetParam());
-  const std::string okFile = std::get<1>(GetParam());
-  const std::uint64_t numVertices = std::get<2>(GetParam());
-
-  uint64_t expected_tc = get_expected_TC(okFile);
-
-  pando::Array<char> filename;
-  EXPECT_EQ(pando::Status::Success, filename.initialize(elFile.size()));
-  for (uint64_t i = 0; i < elFile.size(); i++)
-    filename[i] = elFile[i];
-
-  e2e_tc_test(expected_tc, filename, numVertices, false);
+  e2e_tc_test(expected_tc, filename, num_vertices, true, tc_chunk);
 
   filename.deinitialize();
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    SmallFiles, TriangleCount,
-    ::testing::Values(std::make_tuple("/pando/graphs/rmat_571919_seed1_scale5_nV32_nE153.el")));
+    SmallFiles, TriangleCountChunking,
+    ::testing::Values(std::make_tuple("/pando/graphs/rmat_571919_seed1_scale5_nV32_nE153.el", 32,
+                                      401, TC_CHUNK::NO_CHUNK),
+                      std::make_tuple("/pando/graphs/rmat_571919_seed1_scale5_nV32_nE153.el", 32,
+                                      401, TC_CHUNK::CHUNK_VERTICES),
+                      std::make_tuple("/pando/graphs/rmat_571919_seed1_scale5_nV32_nE153.el", 32,
+                                      401, TC_CHUNK::CHUNK_EDGES)));
+
+// Chunking not avail for DACSR
+class TriangleCountDACSR
+    : public ::testing::TestWithParam<std::tuple<const char*, uint64_t, uint64_t>> {};
+TEST_P(TriangleCountDACSR, BasicDA) {
+  const std::string elFile = std::get<0>(GetParam());
+  const uint64_t num_vertices = std::get<1>(GetParam());
+  const uint64_t expected_tc = std::get<2>(GetParam());
+
+  pando::Array<char> filename;
+  EXPECT_EQ(pando::Status::Success, filename.initialize(elFile.size()));
+  for (uint64_t i = 0; i < elFile.size(); i++)
+    filename[i] = elFile[i];
+
+  e2e_tc_test(expected_tc, filename, num_vertices, false, TC_CHUNK::NO_CHUNK);
+
+  filename.deinitialize();
+}
+INSTANTIATE_TEST_SUITE_P(SmallFiles, TriangleCountDACSR,
+                         ::testing::Values(std::make_tuple(
+                             "/pando/graphs/rmat_571919_seed1_scale5_nV32_nE153.el", 32, 401)));
