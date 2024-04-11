1371a082e7aed90c5cb0035ec4f77bea82671d6e
AdityaAtulTewari <adityaatewari@gmail.com>
2024-04-11 11:10:23 -0700

    Now builds tests and runs the simple one (#56)
    * Move bfs around and try to build tests
    
    * Basic testing works
    
    ---------
    
    Co-authored-by: ywwu928 <ywwu928@utexas.edu>

--- microbench/bfs/test/test_sssp.cpp
+++ microbench/bfs/test/test_sssp.cpp
@@ -10,32 +10,38 @@
 
 #include <pando-bfs-galois/sssp.hpp>
 #include <pando-lib-galois/containers/dist_array.hpp>
+#include <pando-lib-galois/containers/host_local_storage.hpp>
+#include <pando-lib-galois/containers/thread_local_vector.hpp>
 #include <pando-lib-galois/graphs/dist_array_csr.hpp>
 #include <pando-lib-galois/loops/do_all.hpp>
 #include <pando-rt/containers/vector.hpp>
 #include <pando-rt/pando-rt.hpp>
 #include <pando-rt/sync/notification.hpp>
 
+struct Edge {
+  std::uint64_t dst;
+};
+
 TEST(SSSP, FullyConnected) {
   constexpr std::uint64_t SIZE = 100;
   galois::DistArray<std::uint64_t> array;
-  pando::Vector<pando::Vector<std::uint64_t>> vec;
+  pando::Vector<pando::Vector<Edge>> vec;
   EXPECT_EQ(vec.initialize(SIZE), pando::Status::Success);
-  for (pando::GlobalRef<pando::Vector<std::uint64_t>> edges : vec) {
-    pando::Vector<std::uint64_t> inner;
+  for (pando::GlobalRef<pando::Vector<Edge>> edges : vec) {
+    pando::Vector<Edge> inner;
     EXPECT_EQ(inner.initialize(0), pando::Status::Success);
     edges = inner;
   }
 
   galois::doAll(
-      vec, +[](pando::GlobalRef<pando::Vector<std::uint64_t>> innerRef) {
-        pando::Vector<std::uint64_t> inner = innerRef;
+      vec, +[](pando::GlobalRef<pando::Vector<Edge>> innerRef) {
+        pando::Vector<Edge> inner = innerRef;
         for (std::uint64_t i = 0; i < SIZE; i++) {
-          EXPECT_EQ(inner.pushBack(i), pando::Status::Success);
+          EXPECT_EQ(inner.pushBack(Edge{i}), pando::Status::Success);
         }
         innerRef = inner;
       });
-  using Graph = galois::DistArrayCSR<std::uint64_t, std::uint64_t>;
+  using Graph = galois::DistArrayCSR<std::uint64_t, Edge>;
   Graph graph;
 
   graph.initialize(vec);
@@ -47,7 +53,7 @@ TEST(SSSP, FullyConnected) {
   }
 
   using TopID = typename galois::graph_traits<Graph>::VertexTopologyID;
-  using LoopStructure = galois::PerHost<pando::Vector<TopID>>;
+  using LoopStructure = galois::HostLocalStorage<pando::Vector<TopID>>;
   LoopStructure phbfs{};
   PANDO_CHECK(phbfs.initialize());
 
@@ -56,14 +62,16 @@ TEST(SSSP, FullyConnected) {
         PANDO_CHECK(fmap(vecRef, initialize, 2));
         liftVoid(vecRef, clear);
       }));
-  galois::PerThreadVector<TopID> next;
+  galois::ThreadLocalVector<TopID> next;
 
   PANDO_CHECK(next.initialize());
 
-  EXPECT_EQ(galois::SSSP(graph, 0, next, phbfs), pando::Status::Success);
+  EXPECT_EQ(galois::SSSP_DLCSR(graph, 0, next, phbfs), pando::Status::Success);
   EXPECT_EQ(graph.getData(0), static_cast<std::uint64_t>(0));
   for (std::uint64_t i = 1; i < SIZE; i++) {
     EXPECT_EQ(graph.getData(i), static_cast<std::uint64_t>(1));
   }
+  phbfs.deinitialize();
+  next.deinitialize();
   graph.deinitialize();
 }
