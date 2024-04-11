ea32157deef1b0fe01143bdd913f1aea492742e6
pingle14 <71203810+pingle14@users.noreply.github.com>
2024-04-10 22:41:57 -0500

    Add TC Chunked Vertices (#49)
    * Add TC-non-chunking and basic test
    
    * update headers
    
    * Add TC-non-chunking and basic test
    
    * Add TC-chunk-edges
    
    * Add tc-chunk-vertices

--- microbench/triangle-counting/src/tc_algos.cpp
+++ microbench/triangle-counting/src/tc_algos.cpp
@@ -119,6 +119,76 @@ void tc_chunk_edges(pando::GlobalPtr<GraphDL> graph_ptr,
   work_remaining.deinitialize();
 }
 
+/**
+ * @brief Runs Chunked Vertices Triangle Counting Algorithm on DistLocalCSRs (GraphDL)
+ *
+ * @param[in] graph_ptr Pointer to the in-memory graph
+ * @param[in] final_tri_count Thread-safe counter
+ */
+void tc_chunk_vertices(pando::GlobalPtr<GraphDL> graph_ptr,
+                       galois::DAccumulator<uint64_t> final_tri_count) {
+  GraphDL graph = *graph_ptr;
+  uint64_t num_hosts = static_cast<std::uint64_t>(pando::getPlaceDims().node.id);
+  uint64_t query_sz = 1;
+  uint64_t iters = 0;
+
+  galois::DAccumulator<uint64_t> work_remaining{};
+  PANDO_CHECK(work_remaining.initialize());
+
+  // Initialize vertexlist offsets to 0
+  galois::HostIndexedMap<uint64_t> per_host_iterator_offsets{};
+  PANDO_CHECK(per_host_iterator_offsets.initialize());
+  galois::doAll(
+      per_host_iterator_offsets, +[](pando::GlobalRef<std::uint64_t> host_vertex_iter_offset_ref) {
+        host_vertex_iter_offset_ref = 0;
+      });
+
+  do {
+    work_remaining.reset();
+    auto state = galois::make_tpl(graph_ptr, query_sz, work_remaining, final_tri_count);
+    galois::doAll(
+        state, per_host_iterator_offsets,
+        +[](decltype(state) state, pando::GlobalRef<std::uint64_t> host_vertex_iter_offset_ref) {
+          auto [graph_ptr, query_sz, work_remaining, final_tri_count] = state;
+          GraphDL graph = *graph_ptr;
+          auto lcsr = graph.getLocalCSR();
+          uint64_t host_vertex_iter_offset = host_vertex_iter_offset_ref;
+
+          auto inner_state = galois::make_tpl(graph_ptr, final_tri_count);
+          galois::doAll(
+              inner_state, fmap(lcsr, vertices, host_vertex_iter_offset, query_sz),
+              +[](decltype(inner_state) inner_state, typename GraphDL::VertexTopologyID v0) {
+                auto [graph_ptr, final_tri_count] = inner_state;
+                GraphDL graph = *graph_ptr;
+
+                // Degree Filtering Optimization
+                uint64_t v0_degree = graph.getNumEdges(v0);
+                if (v0_degree < (TC_EMBEDDING_SZ - 1))
+                  return;
+
+                edge_tc_counting<GraphDL>(graph_ptr, v0, graph.edges(v0), final_tri_count);
+              });
+
+          // Move iter offset
+          uint64_t lcsr_num_vertices = fmap(lcsr, size);
+          host_vertex_iter_offset += query_sz;
+          if (host_vertex_iter_offset < lcsr_num_vertices)
+            work_remaining.increment();
+          host_vertex_iter_offset_ref = host_vertex_iter_offset;
+        });
+
+    uint64_t current_count = final_tri_count.reduce();
+    std::cout << "After Iter " << iters << " found " << current_count << " triangles.\n";
+    final_tri_count.reset();
+    final_tri_count.add(current_count);
+    iters++;
+    query_sz <<= 1;
+  } while (work_remaining.reduce());
+
+  work_remaining.deinitialize();
+  per_host_iterator_offsets.deinitialize();
+}
+
 // #####################################################################
 //                        TC GRAPH HBMAINS
 // #####################################################################
@@ -151,12 +221,12 @@ void HBGraphDL(pando::Place thisPlace, pando::Array<char> filename, int64_t num_
   PANDO_MEM_STAT_NEW_KERNEL("TC_DFS_Algo Start");
 
   switch (tc_chunk) {
+    case TC_CHUNK::CHUNK_VERTICES:
+      tc_chunk_vertices(graph_ptr, final_tri_count);
+      break;
     case TC_CHUNK::CHUNK_EDGES:
       tc_chunk_edges(graph_ptr, final_tri_count);
       break;
-    // case TC_CHUNK::CHUNK_VERTICES:
-    //   tc_chunk_vertices(graph_ptr, final_tri_count);
-    //   break;
     default:
       tc_no_chunk<GraphDL>(graph_ptr, final_tri_count);
       break;
