4f79f206ef81ba0d79b921408a2c57676fb50305
Prachi Ingle <prachi.ingle@utexas.edu>
2024-04-11 02:33:53 +0000

    Fixes

--- microbench/triangle-counting/test/test_tc.cpp
+++ microbench/triangle-counting/test/test_tc.cpp
@@ -12,22 +12,22 @@ uint64_t get_expected_TC(const std::string okFile) {
   return expected_tc;
 }
 
-void e2e_tc_DFS_test(uint64_t expected_tc, pando::Array<char> filename, uint64_t numVertices,
-                     bool load_balanced_graph) {
+void e2e_tc_test(uint64_t expected_tc, pando::Array<char> filename, uint64_t numVertices,
+                 bool load_balanced_graph) {
   galois::DAccumulator<uint64_t> final_tri_count;
   EXPECT_EQ(final_tri_count.initialize(), pando::Status::Success);
   pando::Notification necessary;
   PANDO_CHECK(pando::executeOn(pando::Place{pando::NodeIndex{0}, pando::anyPod, pando::anyCore},
-                               &HBMainDFS, necessary.getHandle(), filename, numVertices,
+                               &HBMainTC, necessary.getHandle(), filename, numVertices,
                                load_balanced_graph, final_tri_count));
   necessary.wait();
   EXPECT_EQ(final_tri_count.reduce(), expected_tc);
   final_tri_count.deinitialize();
 }
 
-class TriangleCountDFS
+class TriangleCount
     : public ::testing::TestWithParam<std::tuple<const char*, const char*, std::uint64_t>> {};
-TEST_P(TriangleCountDFS, BasicDL) {
+TEST_P(TriangleCount, BasicDL) {
   const std::string elFile = std::get<0>(GetParam());
   const std::string okFile = std::get<1>(GetParam());
   const std::uint64_t numVertices = std::get<2>(GetParam());
@@ -39,12 +39,12 @@ TEST_P(TriangleCountDFS, BasicDL) {
   for (uint64_t i = 0; i < elFile.size(); i++)
     filename[i] = elFile[i];
 
-  e2e_tc_DFS_test(expected_tc, filename, numVertices, true);
+  e2e_tc_test(expected_tc, filename, numVertices, true);
 
   filename.deinitialize();
 }
 
-TEST_P(TriangleCountDFS, BasicDA) {
+TEST_P(TriangleCount, BasicDA) {
   const std::string elFile = std::get<0>(GetParam());
   const std::string okFile = std::get<1>(GetParam());
   const std::uint64_t numVertices = std::get<2>(GetParam());
@@ -56,23 +56,11 @@ TEST_P(TriangleCountDFS, BasicDA) {
   for (uint64_t i = 0; i < elFile.size(); i++)
     filename[i] = elFile[i];
 
-  e2e_tc_DFS_test(expected_tc, filename, numVertices, false);
+  e2e_tc_test(expected_tc, filename, numVertices, false);
 
   filename.deinitialize();
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    SmallFiles, TriangleCountDFS,
-    ::testing::Values(
-        // std::make_tuple("/pando/graphs/rmat_571919_seed1_scale5_nV32_nE153.el",
-        // "/pando/ok/rmat_571919_seed1_scale5_nV32_nE153.el-32.ok", 32),
-        // std::make_tuple("/pando/graphs/rmat_571919_seed1_scale6_nV64_nE403.el",
-        // "/pando/ok/rmat_571919_seed1_scale6_nV64_nE403.el-64.ok", 64),
-        // std::make_tuple("/pando/graphs/rrmat_571919_seed1_scale7_nV128_nE942.el",
-        // "/pando/ok/rmat_571919_seed1_scale7_nV128_nE942.el-128.ok", 128),
-        std::make_tuple("/pando/graphs/rmat_571919_seed1_scale8_nV256_nE2144.el",
-                        "/pando/ok/rmat_571919_seed1_scale8_nV256_nE2144.el-256.ok", 256)));
-// std::make_tuple("/pando/graphs/rmat_571919_seed1_scale9_nV512_nE4736.el",
-// "/pando/ok/rmat_571919_seed1_scale9_nV512_nE4736.el-512.ok", 512),
-// std::make_tuple("/pando/graphs/rmat_571919_seed1_scale10_nV1024_nE10447.el",
-// "/pando/ok/rmat_571919_seed1_scale10_nV1024_nE10447.el-1024.ok", 1024)
+    SmallFiles, TriangleCount,
+    ::testing::Values(std::make_tuple("/pando/graphs/rmat_571919_seed1_scale5_nV32_nE153.el")));
