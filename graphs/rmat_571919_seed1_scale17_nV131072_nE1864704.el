fde3553259084b7cb46923e7978aab99bc160dfd
AdityaAtulTewari <adityaatewari@gmail.com>
2024-04-16 20:02:30 +0000

    Larger graphs

---
+++ graphs/rmat_571919_seed1_scale17_nV131072_nE1864704.el
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e4e3ce5c0692f6b94bc6531e08639d849c4430c78347bb42cfabb6921d213d29
+size 20475215
---
+++ graphs/rmat_571919_seed1_scale18_nV262144_nE3806162.el
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:342dae5aaa2c7506453e106ee055e5db8c17b5139d5c814eb030267149243192
+size 44645077
