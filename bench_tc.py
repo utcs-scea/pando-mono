a96d378310be7036db1b602066fc79d14b3bab27
Prachi Ingle <prachi.ingle@utexas.edu>
2024-04-22 17:14:51 +0000

    Updated bench scripts

--- bench_tc.py
+++ bench_tc.py
@@ -19,47 +19,16 @@ rmat_files = {
     8: "graphs/rmat_571919_seed1_scale8_nV256_nE2144.el",
     9: "graphs/rmat_571919_seed1_scale9_nV512_nE4736.el",
     10: "graphs/rmat_571919_seed1_scale10_nV1024_nE10447.el",
-    11: "graphs/rmat_571919_seed1_scale11_nV2048_nE22601.el"
+    11: "graphs/rmat_571919_seed1_scale11_nV2048_nE22601.el",
+    12: "graphs/rmat_571919_seed1_scale12_nV4096_nE48335.el",
+    13: "graphs/rmat_571919_seed1_scale13_nV8192_nE102016.el",
+    14: "graphs/rmat_571919_seed1_scale14_nV16384_nE213350.el"
 }
 
 chunk_modes = ["no_chunk", "chunk_vert", "chunk_edge"]
+graph_types = ["DLCSR", "MDLCSR", "DACSR"]
 
-def compile():
-    print("Compiling ... ", end=" ")
-    subprocess.call(["make", "setup"])
-    subprocess.call(["make", "-C", "dockerbuild", "-j8"])
-    print("DONE")
-
-def clean():
-    print("Cleaning ... ", end=" ")
-    subprocess.call(["rm", "-rf", "dockerbuild"])
-    print("DONE")
-
-def run_cmd(command_line, env={}):
-    args = shlex.split(command_line)
-    print(args)
-    proc = subprocess.Popen(args, stdout=subprocess.PIPE)
-    try:
-        outs = subprocess.check_output(args, env=env, stderr=subprocess.STDOUT)
-        return outs.decode().split("\n")
-    except subprocess.CalledProcessError as error:
-        errorMessage = (
-            ">>> Error while executing:\n"
-            + command_line
-            + "\n>>> Returned with error:\n"
-            + str(error.output.decode())
-        )
-        with open("err.txt", "w+") as f:
-            print("WRITING ERR TO FILE ... ")
-            f.write(errorMessage)
-        assert False
-    except subprocess.TimeoutExpired:
-        print("\t TIMEOUT")
-        proc.kill()
-        assert False
-    return None
-
-def collect_trace_DLCSR(scale=5, chunk_mode=0, num_hosts=2, num_cores=32, run=False, graph_type = 0):
+def collect_traces_RMAT(num_hosts, num_cores, graph_mode, chunk_mode, scale, output_dir='data'):
     assert PXNS_TIMES_CORES % num_hosts == 0, f"PXNS_TIMES_CORES = {PXNS_TIMES_CORES} should b divisible by num_hosts = {num_hosts}"
     assert PXNS_TIMES_PE % num_hosts == 0, f"num_hosts = {num_hosts} should be even"
     pe = PXNS_TIMES_PE // num_hosts
@@ -71,7 +40,9 @@ def collect_trace_DLCSR(scale=5, chunk_mode=0, num_hosts=2, num_cores=32, run=Fa
     # pe = localhost:32 / num_hosts
 
     # Create place for trace files
-    fn = f"data/DLCSR/host{num_hosts}/{chunk_modes[chunk_mode]}/scale{scale}"
+    input_graph = rmat_files[scale]
+    num_input_vertices = 2**scale
+    fn = f"{output_dir}/{graph_types[graph_mode]}/host{num_hosts}/{chunk_modes[chunk_mode]}/scale{scale}"
     print(f"\t *** Creating {fn}")
     if not os.path.exists(fn):
         os.makedirs(fn)
@@ -79,33 +50,47 @@ def collect_trace_DLCSR(scale=5, chunk_mode=0, num_hosts=2, num_cores=32, run=Fa
     assert os.path.exists(fn), f"OS should now see a path to {fn}"
 
     # Generate trace files
-    cmd0 = f'MPIRUN_CMD="mpirun -np %N -host localhost:32 --mca btl self,vader,tcp --map-by slot:PE={pe} --use-hwthread-cpus %P %A" PANDO_PREP_MAIN_NODE=17179869184 '
-    cmd0 += f"./scripts/preprun_mpi.sh -c {num_cores} -n {num_hosts} {executable} -i {rmat_files[scale]} -v {2**scale} -c {chunk_mode} -g {graph_type}"
+    cmd0 = f'MPIRUN_CMD="mpirun -np %N -host localhost:{PXNS_TIMES_PE} --mca btl self,vader,tcp --map-by slot:PE={pe} --use-hwthread-cpus %P %A" PANDO_PREP_MAIN_NODE=17179869184 '
+    cmd0 += f"./scripts/preprun_mpi.sh -c {num_cores} -n {num_hosts} {executable} -i {input_graph} -v {num_input_vertices} -c {chunk_mode} -g {graph_mode}"
     # Move trace files into dir
     cmd1 = f"mv *.trace {fn}"
-    if run:
-        run_cmd(cmd0, env=env)
-        run_cmd(cmd1)
 
     return [cmd0, cmd1]
 
 
-
-def bench_DLCSR(num_hosts=8):
+def experiment_chunk_DLCSR_RMAT(num_hosts, num_cores, output_dir='data'):
     print("*******************************************************")
-    print(f"          Collecting Benchmarks for DLCSR ...")
+    print(f"          Collecting Benchmarks for CHUNKING on DLCSR ...")
     print("*******************************************************")
     all_cmds = []
     for scale in rmat_files:
         msg = f'"*** scale = {scale}, fn = {rmat_files[scale]} ***"'
         all_cmds.append(f"echo {msg}")
         for c in range(len(chunk_modes)):
-            cmds = collect_trace_DLCSR(scale, c, num_hosts)
+            cmds = collect_traces_RMAT(num_hosts, num_cores, 0, c, scale, output_dir=f'{output_dir}/expChunk')
             all_cmds += cmds
 
-    with open("bench_tc.sh", "w") as file:
+    with open("experiment_chunk.sh", "w") as file:
         for line in all_cmds:
             file.write(f"{line}\n")
+
+def experiment_graph_type_RMAT(num_hosts, num_cores, output_dir='data'):
+    print("*******************************************************")
+    print(f"          Collecting Benchmarks for GRAPH TYPES ...")
+    print("*******************************************************")
+    all_cmds = []
+    for scale in rmat_files:
+        msg = f'"*** scale = {scale}, fn = {rmat_files[scale]} ***"'
+        all_cmds.append(f"echo {msg}")
+        for g in range(len(graph_types)):
+            input_graph = rmat_files[scale]
+            cmds = collect_traces_RMAT(num_hosts, num_cores, g, 0, scale, output_dir=f'{output_dir}/expGT')
+            all_cmds += cmds
+
+    with open("experiment_graph_type.sh", "w") as file:
+        for line in all_cmds:
+            file.write(f"{line}\n")
+
 
 
 
@@ -118,25 +103,41 @@ parser.add_argument(
     help=f"Enter number of hosts",
     type=int,
     required=True,
-    default=8,
 )
-# parser.add_argument(
-#     "-c",
-#     "--numCores",
-#     action="store",
-#     help=f"Enter number of cores per host",
-#     type=int,
-#     required=False,
-#     default=8,
-# )
+parser.add_argument(
+    "-c",
+    "--numCores",
+    action="store",
+    help=f"Enter number of cores",
+    type=int,
+    required=True,
+)
+parser.add_argument(
+    "-e",
+    "--experiment",
+    help=f"Choose experiment: True: experiment_chunk_DLCSR_RMAT, False: experiment_graph_type_RMAT",
+    action='store_true',
+    required=False,
+    default=False
+)
+parser.add_argument(
+    "-o",
+    "--outputDir",
+    help=f"Choose outputDir",
+    action="store",
+    type=str,
+    required=True
+)
 
 args = vars(parser.parse_args())
 num_hosts = int(args["numHosts"])
-# num_cores = int(args["numCores"])
+num_cores = int(args["numCores"])
+output_dir = str(args['outputDir'])
+experiment = experiment_chunk_DLCSR_RMAT if bool(args['experiment']) else experiment_graph_type_RMAT
 print("*" * 42)
-print("*" + " " * 10 + f"Benching TC on {num_hosts} hosts")
+print("*" + " " * 10 + f"Generating bench scripts: TC on num_hosts: {num_hosts}, num_cores: {num_cores}, outputDir: {output_dir}")
 print("*" * 42)
 
 assert num_hosts > 0, f"ERROR: Num hosts = {num_hosts} should be > 0"
-# assert num_cores > 0, f"ERROR: Num hosts = {num_cores} should be > 0"
-bench_DLCSR(num_hosts=num_hosts)
+assert num_cores > 0, f"ERROR: Num hosts = {num_cores} should be > 0"
+experiment(num_hosts=num_hosts, num_cores=num_cores, output_dir=output_dir)
--- tacc_setup.sh
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2023. University of Texas at Austin. All rights reserved.
-
-module load intel/19.1.1
-module load impi/19.0.9
-module load python3/3.9.7
-. ./share/spack/setup-env.sh
-spack compiler find
-spack install -j$(nproc) gasnet@2023.3.0%intel@19.1.1.217 conduits=smp,mpi target=x86_64
