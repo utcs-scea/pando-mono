545e7e7b8c6ce20078285c8fc7b43e44df3afbea
AdityaAtulTewari <adityaatewari@gmail.com>
2024-04-30 18:45:30 +0000

    Revert "Updates to make description meaningful (#83)"
    This reverts commit d560fd666eda2c0415322f0dd12757818e6ddacd.

---
+++ bench_tc.py
@@ -0,0 +1,210 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2023. University of Texas at Austin. All rights reserved.
+
+import shlex, subprocess
+import argparse
+import os
+
+# Generates bash script to run and collect benchmarks
+env = {"PANDO_PREP_MAIN_NODE": "17179869184"}
+
+
+rmat_files = {
+    3:"graphs/kron_default_3_out.el",
+    4:"graphs/kron_default_4_out.el"
+    # 5: "graphs/rmat_571919_seed1_scale5_nV32_nE153.el",
+    # 6: "graphs/rmat_571919_seed1_scale6_nV64_nE403.el",
+    # 7: "graphs/rmat_571919_seed1_scale7_nV128_nE942.el",
+    # 8: "graphs/rmat_571919_seed1_scale8_nV256_nE2144.el",
+    # 9: "graphs/rmat_571919_seed1_scale9_nV512_nE4736.el",
+    # 10: "graphs/rmat_571919_seed1_scale10_nV1024_nE10447.el"
+    # 11: "graphs/rmat_571919_seed1_scale11_nV2048_nE22601.el",
+    # 12: "graphs/rmat_571919_seed1_scale12_nV4096_nE48335.el",
+    # 13: "graphs/rmat_571919_seed1_scale13_nV8192_nE102016.el",
+    # 14: "graphs/rmat_571919_seed1_scale14_nV16384_nE213350.el"
+}
+
+chunk_modes = ["no_chunk", "chunk_vert", "chunk_edge"]
+graph_types = ["DLCSR", "MDLCSR", "DACSR"]
+machines = {'languedoc':64, 'zerberus':32, 'tacc':128}
+
+def collect_traces_RMAT(num_hosts, graph_mode, chunk_mode, scale, machine, output_dir='data', exec2run='tc', build='dockerbuild'):
+    if build == 'dockerbuild-drv':
+        output_dir += "/DRV"
+
+    compute_units = machines[machine]
+    assert compute_units % num_hosts == 0, f"compute_units = {compute_units} should b divisible by num_hosts = {num_hosts}"
+    pe = compute_units // num_hosts
+    if machine == 'languedoc':
+        pe //= 2
+    num_cores = compute_units // num_hosts
+
+    # Create place for trace files
+    input_graph = rmat_files[scale]
+    num_input_vertices = 2**scale
+    fn = f"{output_dir}/{graph_types[graph_mode]}/host{num_hosts}/{chunk_modes[chunk_mode]}/scale{scale}"
+
+    if build == 'dockerbuild' and not os.path.exists(fn):
+        print(f"\t *** Creating {fn}")
+        os.makedirs(fn)
+        assert os.path.exists(fn), f"OS should now see a path to {fn}"
+
+    executable = f"./{build}/microbench/triangle-counting/src/{exec2run}"
+    cmd0 = ""
+    if build=='dockerbuild-drv':
+        cmd0 = f"./scripts/run-drv.sh -n {num_hosts} -c {num_cores} ./dockerbuild-drv/microbench/triangle-counting/src/libtc.so "
+    else:
+        # Generate trace files
+        cmd0 = f'MPIRUN_CMD="mpirun -np %N -host localhost:{compute_units} --mca btl self,vader,tcp --map-by slot:PE={pe} --use-hwthread-cpus %P %A" PANDO_PREP_MAIN_NODE=17179869184 '
+        cmd0 += f"./scripts/preprun_mpi.sh -c {num_cores} -n {num_hosts} {executable} "
+
+    cmd0 += f"-i {input_graph} -v {num_input_vertices} -c {chunk_mode} -g {graph_mode}"
+
+    if build=='dockerbuild-drv':
+        cmd0 += " | grep Time"
+
+    # Move trace files into dir
+    cmd1 = f"mv *.trace {fn}"
+
+    return [cmd0, cmd1]
+
+
+def create_slurm(num_hosts, graph_mode, chunk_mode, scale, machine, output_dir='data', time='01:00:00'):
+    input_graph = rmat_files[scale]
+    num_input_vertices = 2**scale
+    fn = f"{output_dir}/{graph_types[graph_mode]}/host{num_hosts}/{chunk_modes[chunk_mode]}/scale{scale}"
+    with open('job2run.slurm', 'w') as file:
+        sbatch_settings = f'#!/bin/bash\n#SBATCH -J {fn}\n#SBATCH -o {fn}.out\n#SBATCH -e {fn}.err\n#SBATCH -t {time}\n#SBATCH -N 1\n#SBATCH -n 1\n#SBATCH -c 128\n#SBATCH --exclusive\n#SBATCH --mail-type=none\n#SBATCH --mail-user=prachi.ingle@utexas.edu\n#SBATCH -p normal\n'
+        file.write(f"{sbatch_settings}\n")
+
+        echos = f'''echo "SLURM_JOB_ID=\$SLURM_JOB_ID"\necho "TIME={time}"\n'''
+        file.write(f"{echos}\n")
+
+        file.write(". ./scripts/tacc_env.sh")
+
+        print(f"\t *** Creating {fn}")
+        if not os.path.exists(fn):
+            os.makedirs(fn)
+
+
+
+
+
+
+def experiment_chunk_DLCSR_RMAT(num_hosts, machine, output_dir='data', build='dockerbuild'):
+    print("*******************************************************")
+    print(f"          Collecting Benchmarks for CHUNKING on DLCSR ...")
+    print("*******************************************************")
+    all_cmds = []
+    for scale in rmat_files:
+        msg = f'"*** scale = {scale}, fn = {rmat_files[scale]} ***"'
+        all_cmds.append(f"echo {msg}")
+        for c in range(len(chunk_modes)):
+            cmds = collect_traces_RMAT(num_hosts, 0, c, scale, machine, output_dir=f'{output_dir}/expChunk', build=build)
+            all_cmds += cmds
+
+    with open(f"experiment_{build}_chunk_{machine}_hosts{num_hosts}.sh", "w") as file:
+        for line in all_cmds:
+            file.write(f"{line}\n")
+
+def experiment_graph_type_RMAT(num_hosts, machine, output_dir='data',build='dockerbuild'):
+    print("*******************************************************")
+    print(f"          Collecting Benchmarks for GRAPH TYPES ...")
+    print("*******************************************************")
+    all_cmds = []
+    for scale in rmat_files:
+        msg = f'"*** scale = {scale}, fn = {rmat_files[scale]} ***"'
+        all_cmds.append(f"echo {msg}")
+        for g in range(len(graph_types)):
+            input_graph = rmat_files[scale]
+            cmds = collect_traces_RMAT(num_hosts, g, 0, scale, machine, output_dir=f'{output_dir}/expGT', build=build)
+            all_cmds += cmds
+
+    with open(f"experiment_{build}_graph_type_{machine}_hosts{num_hosts}.sh", "w") as file:
+        for line in all_cmds:
+            file.write(f"{line}\n")
+
+
+def experiment_graph_construction_RMAT(num_hosts, machine, output_dir='data', exec2run='graph_compare',build='dockerbuild'):
+    print("*******************************************************")
+    print(f"          Collecting Benchmarks for GRAPH TYPES ...")
+    print("*******************************************************")
+    all_cmds = []
+    for scale in rmat_files:
+        msg = f'"*** scale = {scale}, fn = {rmat_files[scale]} ***"'
+        all_cmds.append(f"echo {msg}")
+        for g in range(len(graph_types)):
+            input_graph = rmat_files[scale]
+            cmds = collect_traces_RMAT(num_hosts, g, 0, scale, machine, output_dir=f'{output_dir}/exp_graph_construc', exec2run=exec2run, build=build)
+            all_cmds += cmds
+
+    with open(f"experiment_{build}_graph_construc_{machine}_hosts{num_hosts}.sh", "w") as file:
+        for line in all_cmds:
+            file.write(f"{line}\n")
+
+
+# ------------------- RUN ---------------------
+
+parser = argparse.ArgumentParser(prog="BenchMarkTC", description="Benchmarks TC on PREP")
+parser.add_argument(
+    "-n",
+    "--numHosts",
+    action="store",
+    help=f"Enter number of hosts",
+    type=int,
+    required=True,
+)
+parser.add_argument(
+    "-b",
+    "--build",
+    action="store",
+    help=f"Enter build (default = dockerbuild)",
+    type=str,
+    required=False,
+)
+parser.add_argument(
+    "-e",
+    "--experiment",
+    help=f"Choose experiment: True: experiment_chunk_DLCSR_RMAT, False: experiment_graph_type_RMAT",
+    action='store_true',
+    required=False,
+    default=False
+)
+parser.add_argument(
+    "-o",
+    "--outputDir",
+    help=f"Choose outputDir",
+    action="store",
+    type=str,
+    required=True
+)
+parser.add_argument(
+    "-m",
+    "--machine",
+    help=f"Specify machine: Options = {machines.keys()}",
+    choices=machines,
+    action='store',
+    type=str,
+    required=True,
+    default='zerberus'
+)
+
+args = vars(parser.parse_args())
+num_hosts = int(args["numHosts"])
+output_dir = str(args['outputDir'])
+machine = str(args['machine'])
+build = str(args['build'])
+
+assert num_hosts > 0, f"ERROR: Num hosts = {num_hosts} should be > 0"
+assert machine in machines, f'ERROR: Dont recognize machine: {machine}'
+
+compute_units = machines[machine]
+
+print("*" * 42)
+print("*" + " " * 10 + f"Generating bench scripts for {machine} (compute_units = {compute_units}): TC on num_hosts: {num_hosts}, num_cores: {compute_units//num_hosts}, outputDir: {output_dir}")
+print("*" * 42)
+
+
+experiment_chunk_DLCSR_RMAT(num_hosts=num_hosts, output_dir=output_dir, machine=machine,build=build)
+experiment_graph_type_RMAT(num_hosts=num_hosts, output_dir=output_dir, machine=machine,build=build)
+experiment_graph_construction_RMAT(num_hosts=num_hosts, output_dir=output_dir, machine=machine,build=build)
