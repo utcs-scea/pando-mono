313d3295e62887fae0995fc5c14b63f6dfbba3c8
AdityaAtulTewari <adityaatewari@gmail.com>
2024-04-30 12:20:48 -0700

    Lower bound with functors. (#84)
    * Search.hpp
    
    * Get rid of extraneous template parameter
    
    * fix compilation errors

--- include/pando-lib-galois/utility/search.hpp
+++ include/pando-lib-galois/utility/search.hpp
@@ -6,19 +6,28 @@
 
 namespace galois {
 
-template <typename RandomAccessIterator, typename T>
-RandomAccessIterator lower_bound(RandomAccessIterator start, RandomAccessIterator end,
-                                 const T& val) {
+template <typename RandomAccessIterator, typename T, typename F>
+RandomAccessIterator lower_bound(RandomAccessIterator start, RandomAccessIterator end, const T& val,
+                                 F func) {
   RandomAccessIterator lb = start, ub = end;
   while (lb < ub) {
     RandomAccessIterator mid = lb + (ub - lb) / 2;
-    if (*mid < val) {
+    if (func(mid, val)) {
       lb = mid + 1;
     } else {
       ub = mid;
     }
   }
   return ub;
+}
+
+template <typename RandomAccessIterator, typename T>
+RandomAccessIterator lower_bound(RandomAccessIterator start, RandomAccessIterator end,
+                                 const T& val) {
+  return lower_bound(
+      start, end, val, +[](RandomAccessIterator mid, const T& val) {
+        return *mid < val;
+      });
 }
 
 template <typename RandomAccessIterator, typename T>
