0c38f0f01da38b8297e471748cb5ab81dbfe7ea9
AdityaAtulTewari <adityaatewari@gmail.com>
2024-06-13 14:40:47 +0000

    assign and bind to deal with different types of objects

--- include/pando-lib-galois/utility/gptr_monad.hpp
+++ include/pando-lib-galois/utility/gptr_monad.hpp
@@ -54,4 +54,20 @@
     *ptrComputed##__LINE__ = tmp;                                                     \
   } while (0)
 
+#include <pando-rt/memory/global_ptr.hpp>
+
+template <typename T, typename F>
+auto bindFunc(pando::GlobalRef<T> ref, F func) {
+  T obj = ref;
+  auto ret = func(obj);
+  ref = obj;
+  return ret;
+}
+
+template <typename T, typename F>
+auto applyFunc(pando::GlobalRef<T> ref, F func) {
+  T obj = ref;
+  return func(obj);
+}
+
 #endif // PANDO_LIB_GALOIS_UTILITY_GPTR_MONAD_HPP_
